5eb71e8ffbb02c5ff1b576708f8c38923d1acf36
Kevin Y Zhang <133567777+kzmaybe@users.noreply.github.com>
2025-03-11 01:43:57 -0700

    Replaced the card  image url

--- src/cardInfo.js
+++ src/cardInfo.js
@@ -1,7 +1,7 @@
 const cards = [
   {
     name: "AmEx Platinum",
-    image: "/cardImage/AMEX_Plat.webp",
+    image: "https://icm.aexp-static.com/Internet/internationalcardshop/en_sg/images/cards/platinum-card.png",
     description:
       "Earn 5x MR points for flights booked directly with airlines, on up to $500k. Earn 5x MR points for flights and prepaid hotels booked with AmEx Travel. Earn 1x MR points per $1 spent on all other purchases.",
     referralLink:
@@ -10,7 +10,7 @@ const cards = [
 
   {
     name: "Chase Sapphire Preferred(CSP)",
-    image: "/cardImage/Chase_CSP.webp",
+    image: "https://creditcards.chase.com/content/dam/jpmc-marketplace/card-art/sapphire_preferred_card.png",
     description:
       "Earn 5x UR points on travel booked through Chase Travel portal. Earn 3x on dining. Earn 2x UR points on other travel purchases. ",
     referralLink: "https://www.referyourchasecard.com/19q/3AUI20E9R8",
@@ -18,7 +18,7 @@ const cards = [
 
   {
     name: "Chase Freedom Unlimited(CFU)",
-    image: "/cardImage/Chase_CFU.webp",
+    image: "https://www.uscreditcardguide.com/wp-content/uploads/012225-FreedomUnlimited-300x185-1.png",
     description:
       "Earn 5% cash back on travel purchased through Chase Ultimate Rewards, earn 3% cash back on dining (restaurants & delivery) and drugstore.",
     referralLink: "https://www.referyourchasecard.com/18J/Y69XR886NU",
@@ -26,7 +26,7 @@ const cards = [
 
   {
     name: "Chase Freedom Flex(CFF)",
-    image: "/cardImage/Chase_CFF.png",
+    image: "https://creditcards.chase.com/content/dam/jpmc-marketplace/site-assets/images/freedom_cal_freedom_flex_card.png",
     description:
       "Earn 5% cash back in each quarter’s bonus categories, 1% cash back on all other purchases.",
     referralLink: "https://www.referyourchasecard.com/18J/JKLW0JVZ9K",
@@ -34,15 +34,15 @@ const cards = [
 
   {
     name: "Chase United Explorer",
-    image: "/cardImage/Chase_UA.webp",
+    image: "https://creditcards.chase.com/content/dam/jpmc-marketplace/card-art/united_explorer_card.png",
     description:
       "Earn 2x UA miles on United, restaurants and hotels. Earn 1x UA mile per dollar spent on all other purchases.",
     referralLink: "https://www.referyourchasecard.com/215f/Y4O8YC1VLO",
   },
 
   {
-    name: "Travel Rewards Credit Card",
-    image: "/cardImage/wellsFargo_Bilt.webp",
+    name: "WellsFargo Bilt",
+    image: "https://creditcards.wellsfargo.com/W-Card-MarketPlace/v2-19-25/images/Products/Bilt/Bilt_card_D.png",
     description:
       "Earn 1x Bilt points on rent, up to 100k points per calendar year; earn 3x Bilt points on dining; earn 2x Bilt points on travel; and earn 1x Bilt point on other purchases.",
     referralLink: "https://bilt.page/r/T69X-IMMU",
